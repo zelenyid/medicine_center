4c942035d1151ec7fff8cd4d981110e77af0c6cd
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-09-22 12:31:57 +0300

    reset changes

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ from app.database.patient import Patient
 
 if __name__ == '__main__':
     print(Patient.collection_name)
-    print(Patient.insert_obj({'name': 'qwerty', 'phone_number': 6419836}))
+    # print(Patient.insert_obj({'name': 'name', 'phone_number': 38013213}))
     print(Patient.get_all_objects())
