ff9ae5889d4b1e6de2f203d2d5ea1c745df73d5a
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-10-31 22:19:13 +0200

    Add class File Uploader

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
+import codecs
 
 DB_PASSWORD = os.environ['MONGODB_PASSWORD']
 DATABASE_NAME = 'med'
 DB_SOURCE = f'mongodb+srv://user:{DB_PASSWORD}@cluster.7pk1k.mongodb.net/{DATABASE_NAME}?retryWrites=true&w=majority'
+# JSON_KEYS_SERVICE_ACCOUNT = '{}\\{}'.format(os.getcwd(), 'keys_service_account.json')
