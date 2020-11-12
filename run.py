b937fefee9395a248edd0217b8defe23de106488
Nazar <you@example.com>
2020-11-12 22:28:50 +0200

    Add CORS policy

--- run.py
+++ run.py
@@ -3,11 +3,12 @@ from fastapi_jwt_auth.exceptions import AuthJWTException
 from starlette.requests import Request
 from starlette.responses import JSONResponse
 from starlette.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import auth, default
 from app.main import app
 from app.storage.token_storage import RedisTokenStorage
-from config import JwtSettings
+from config import JwtSettings, DEBUG_LOGIN
 
 
 @AuthJWT.load_config
@@ -24,12 +25,14 @@ def check_if_token_in_denylist(decrypted_token):
 
 @app.on_event('startup')
 def startup_event():
-    app.state.redis = RedisTokenStorage()
+    if not DEBUG_LOGIN:
+        app.state.redis = RedisTokenStorage()
 
 
 @app.on_event('shutdown')
 def shutdown_event():
-    app.state.redis.close()
+    if not DEBUG_LOGIN:
+        app.state.redis.close()
 
 
 @app.exception_handler(AuthJWTException)
@@ -43,3 +46,11 @@ def authjwt_exception_handler(request: Request, exc: AuthJWTException):
 app.include_router(default.router)
 app.include_router(auth.router, prefix='/auth', tags=['auth'])
 app.mount("/static", StaticFiles(directory="static"), name='static')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
