02edea434a0624a1fcbc4ae28783e62d228bf48c
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-11-12 16:58:14 +0200

    add endpoints for disease history and scheme for history

--- app/api/disease_history.py
+++ app/api/disease_history.py
@@ -1,37 +1,76 @@
-from datetime import datetime
 import json
 
 from fastapi import APIRouter
 
+from app.validators.schemes.user_schemes import DiseaseHistoryScheme
 
 router = APIRouter()
 
 
-@router.get('/user/{patient_id}/disease_history/')
-def get_disease_histories(patient_id: int):
+@router.get('/{patient_id}')
+async def get_disease_histories(patient_id: int):
     """
     Get list of disease histories of some user
+    TODO: select only "title", "date_updated", "status" from history json
+    TODO: load data from database
     :param patient_id:
-    :return:
+    :return: list of histtories
     """
-    return {'msg': 'List of histories of {}'.format(patient_id)}
+    with open('app/database/test_history.json', 'r') as f:
+        all_histories = json.load(f)
+
+    histories_patient = [history for history in all_histories if
+                         history['patient_id'] == patient_id]
+
+    return histories_patient
 
 
-@router.get('/user/{patient_id}/disease_history/{history_id}')
-def get_history(patient_id: int, history_id: int):
+@router.get('/read_history/{history_id}')
+async def get_history(history_id: int):
     """
     Get history of some patient with some id of history
+    TODO: load data from database
     :param patient_id: id of patient who history we open
+    :param history_id: id of history in the database
+    :return: histories with history_id
+    """
+    with open('app/database/test_history.json', 'r') as f:
+        all_histories = json.load(f)
+
+    history = [history for history in all_histories if history['id'] == history_id][0]
+
+    return history
+
+
+@router.post('/disease_history/add')
+async def add_history(history: DiseaseHistoryScheme):
+    """
+    Add to database new disease history for user
+    TODO: add path to dict from loaded file in form
+    TODO: add data to database
+    :param history: dict of data to add to database
+    :return:
+    """
+    return history
+
+
+@router.put('/disease_history/update/{history_id}')
+async def update_history(history_id: int):
+    """
+    Update history with id - history_id
+    TODO: realize this method
     :param history_id: id of history in the database
     :return:
     """
-    f = open('test_history.json', 'r')
-    histories = json.loads(f.read())
-    f.close()
-
-    return histories[history_id]
+    return {'msg': 'update history'}
 
 
-@router.post('/disease_history/add/')
-async def add_history():
-    pass
+@router.delete('/disease_history/delete/{history_id}')
+async def delete_history(history_id: int):
+    """
+    Delete history with id - history_id
+    TODO: realize this method
+    :param history_id: id of history in the database
+    :return:
+    """
+    return {'msg': 'delete history'}
