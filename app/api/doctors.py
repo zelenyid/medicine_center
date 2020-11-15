e51ba1208796469769964502a78fe5ec188342bc
Dmytro Zelenyi <dzeleniy9@gmail.com>
2020-11-15 22:56:03 +0200

    add combination collections user and patient, user and doctors

--- app/api/doctors.py
+++ app/api/doctors.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from bson.objectid import ObjectId
 
 from app.database.doctor import DoctorsCollection
+from app.database.users import UsersCollection
 
 
 router = APIRouter()
@@ -15,10 +16,10 @@ async def get_doctor_profile(user_id: str):
     :param user_id: Id of doctor in database
     :return: doctor data
     """
-
+    user_data = UsersCollection.to_json(UsersCollection.get_one_obj({'_id': ObjectId(user_id)}))
     doctor_data = DoctorsCollection.to_json(DoctorsCollection.get_one_obj({'user_id': ObjectId(user_id)}))
 
-    return doctor_data
+    return {**doctor_data, **user_data}
 
 
 @router.get('/doctors/')
@@ -27,4 +28,10 @@ async def get_all_doctors():
     Get all doctors from database
     :return: list of doctors
     """
-    return DoctorsCollection.to_json(DoctorsCollection.get_all_objects())
+    list_doctors = DoctorsCollection.to_json(DoctorsCollection.get_all_objects())
+
+    for i in range(len(list_doctors)):
+        user_data = UsersCollection.to_json(UsersCollection.get_one_obj({'_id': ObjectId(list_doctors[i]['user_id'])}))
+        list_doctors[i] = {**list_doctors[i], **user_data}
+
+    return list_doctors
--- app/database/doctor.py
+++ app/database/doctor.py
@@ -8,5 +8,12 @@ class DoctorsCollection(MongoBase):
     db_fields: Sequence[Tuple[str]] = ('_id', 'user_id', 'hospital_id', 'rating', 'positing')
 
 
-# if __name__ == '__main__':
-#     print(DoctorsCollection.get_all_objects())
+if __name__ == '__main__':
+    # DoctorsCollection.insert_obj({
+    #     'user_id': ObjectId('5fb18899202941a87ea630a3'),
+    #     'hospital_id': ObjectId('5fb17cae579548e9cb40cd0c'),
+    #     'rating': '4.5',
+    #     'positing': 'Just doctor'
+    # })
+
+    print(DoctorsCollection.get_all_objects())
